f4fd16ba903045a89176b90f60967547a2bd5d93
simonescoca <simone.scoca@icloud.com>
2023-08-28 17:30:47 +0200

    added relationship with technology model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -30,4 +30,9 @@ class Project extends Model
     {
         return $this->belongsTo(Type::class);
     }
+
+    public function technologies()
+    {
+        return $this->belongsToMany(Technology::class);
+    }
 }
